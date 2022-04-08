3b75fea34904217d1e49c1dafd67cca159760d31
alexb997 <alex_alexandru_997@yahoo.ro>
2022-04-08 12:56:38 +0300

    Junit Test Fake login

---
+++ demo/src/test/java/com/example/back/controller/UserControllerTest.java
@@ -0,0 +1,47 @@
+package com.example.back.controller;
+
+import com.example.back.model.User;
+import com.example.back.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = UserController.class)
+@WithMockUser
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    User mockUser = new User("test","testing");
+
+    @Test
+    public void loginUserTest() throws Exception {
+        Mockito.when(
+                userService.loginUser(Mockito.anyString(),Mockito.anyString())).thenReturn(java.util.Optional.of(mockUser));
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+                "/users/login").accept(
+                MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        System.out.println(result.getResponse());
+        String expected = "{username:test,password:testing}";
+
+        JSONAssert.assertEquals(expected, result.getResponse()
+                .getContentAsString(), false);
+    }
+}
