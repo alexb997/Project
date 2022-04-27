c874260e49cc2f9257b5203e8145f983ce66723c
alexb997 <alex_alexandru_997@yahoo.ro>
2022-04-27 16:35:47 +0300

    Piece container

---
+++ front/src/components/pieces/PieceContainer.js
@@ -0,0 +1,32 @@
+import { Button } from "bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+function PieceContainer(props) {
+  function Capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  return (
+    <Card className="card-bloc">
+      <Card.Img
+        variant="top"
+        src="https://media.istockphoto.com/photos/machine-gears-picture-id1256530089?b=1&k=20&m=1256530089&s=170667a&w=0&h=zrKIAuV70LDRX6i6dMvEYT0YhuDr37Fw_njkxuVbmVg="
+      />
+      <Card.Body>
+        <Card.Title>
+          <Row>{Capitalize(props.piece.name)}</Row>
+        </Card.Title>
+        <Card.Text>
+          <Col className="info-column">
+            <Row>Utility: {Capitalize(props.piece.utility)}</Row>
+            <Row>Type: {Capitalize(props.piece.type)}</Row>
+            <Row>Model: {Capitalize(props.piece.model)}</Row>
+            <Row>Owner: {Capitalize(props.piece.owner)}</Row>
+            <Row>Price: {props.piece.price}</Row>
+          </Col>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default PieceContainer;
