04b818189d9832b2eea4b8acab68cb94d85b20ea
alexb997 <alex_alexandru_997@yahoo.ro>
2022-05-05 15:05:43 +0300

    CarServiceTest addCarTest

--- demo/src/test/java/com/example/back/service/CarServiceTest.java
+++ demo/src/test/java/com/example/back/service/CarServiceTest.java
@@ -22,13 +22,25 @@ public class CarServiceTest {
     @InjectMocks
     private CarService carService;
 
+    private Car mockCar = new Car("Tesla","White","Tesla","Van",null,1200,5,"Electric",7);
+
     @Test
     public void findByIdTest() throws Exception{
-        Car mockCar = new Car("Tesla","White","Tesla","Van",null,1200,5,"Electric",7);
         Mockito.when(carRepository.findById(Mockito.anyString())).thenReturn(Optional.of(mockCar));
 
         Optional<Car> result = carService.findById("SomeID");
         System.out.println(result.toString());
         assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    public void addCarTest() throws Exception{
+        Mockito.when(carRepository.save(Mockito.any(Car.class))).thenReturn(mockCar);
+
+        Car result = carService.addCar(new Car());
+        String expected ="Car{brand='Tesla', color='White', model='Tesla', body='Van', combustible='Electric', owner='null', price=1200, numberDoors=5, cargoVolume=7}";
+
+        assertThat(result.toString()).isEqualTo(expected);
+        System.out.println(result.toString());
     }
 }
