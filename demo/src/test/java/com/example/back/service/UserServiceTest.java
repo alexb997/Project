85cebb7124851131c6930ec1d5d860e7506baf76
alexb997 <alex_alexandru_997@yahoo.ro>
2022-05-05 20:32:36 +0300

    UserServiceTest

---
+++ demo/src/test/java/com/example/back/service/UserServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.back.service;
+
+import com.example.back.model.Pieces;
+import com.example.back.model.User;
+import com.example.back.repository.UserRepository;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    private User mockUser = new User("test","testing");
+
+    @Test
+    public void loginUserTest(){
+        Mockito.when(userRepository.findByUsernameAndPassword(Mockito.anyString(),Mockito.anyString())).thenReturn(Optional.of(mockUser));
+
+        Optional<User> result = userService.loginUser("SomeUser","somePass");
+        System.out.println(result.toString());
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    public void addNewUserTest() throws Exception{
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(mockUser);
+
+        User result = userService.addNewUser(new User());
+        String expected ="User{id='null', username='test', password='testing'}";
+
+        assertThat(result.toString()).isEqualTo(expected);
+        System.out.println(result);
+    }
+
+    @Test
+    public void getUserTest() throws Exception{
+        Mockito.when(userRepository.findByUsername(Mockito.anyString())).thenReturn(Optional.ofNullable(mockUser));
+
+        Optional<User> result = userService.findByUsername("someUser");
+
+        System.out.println(result.toString());
+        assertThat(result).isNotEmpty();
+    }
+
+}
