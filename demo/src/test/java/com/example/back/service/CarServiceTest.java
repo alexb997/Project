bb3f8655c65bfb59c650e40acb7953656450e203
alexb997 <alex_alexandru_997@yahoo.ro>
2022-05-05 17:23:25 +0300

    CarServiceTest deleteTests

--- demo/src/test/java/com/example/back/service/CarServiceTest.java
+++ demo/src/test/java/com/example/back/service/CarServiceTest.java
@@ -11,12 +11,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
@@ -126,4 +132,17 @@ public class CarServiceTest {
         assertThat(result.getTotalElements()).isEqualTo(mockPageCars.getTotalElements());
         System.out.println(result.getTotalElements());
     }
-}
+
+    @Test
+    public void removeByIDTest(){
+        String mockId = "someID";
+        carService.removeCarById(mockId);
+        Mockito.verify(carRepository).deleteById(mockId);
+    }
+
+    @Test
+    public void removeAllTest(){
+        carService.removeAllCars();
+        Mockito.verify(carRepository).deleteAll();
+    }
+};
