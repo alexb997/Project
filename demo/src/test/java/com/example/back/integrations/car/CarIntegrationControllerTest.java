2b8ab889ecb43ca021afa53b3b14b2507a3c54c7
alexb997 <alex_alexandru_997@yahoo.ro>
2022-05-07 18:07:10 +0300

    carControllerTest

--- demo/src/test/java/com/example/back/integrations/car/CarIntegrationControllerTest.java
+++ demo/src/test/java/com/example/back/integrations/car/CarIntegrationControllerTest.java
@@ -2,21 +2,24 @@ package com.example.back.integrations.car;
 
 import com.example.back.controller.CarController;
 import com.example.back.model.Car;
-import org.junit.jupiter.api.Test;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CarIntegrationControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -24,7 +27,10 @@ public class CarIntegrationControllerTest {
     @Autowired
     private CarController carController;
 
+    static String id= StringUtils.EMPTY;
+
     @Test
+    @Order(1)
     public void givenCars_whenGetCars_thenStatus200() throws Exception {
 
         mvc.perform(get("/cars/filter").contentType(MediaType.APPLICATION_JSON))
@@ -36,19 +42,63 @@ public class CarIntegrationControllerTest {
     }
 
     @Test
+    @Order(2)
     public void createCar_thenStatusCreated() throws Exception {
-
-        Car mockCar = new Car("Tesla","White","Tesla","Van",null,1200,5,"Electric",7);
-
         String exampleCarJson ="{\"brand\":\"Tesla\",\"color\":\"White\",\"model\":\"Tesla\",\"body\":\"Van\",\"combustible\":\"Electric\",\"owner\":\"null\",\"price\":\"1200\",\"numberDoors\":\"5\",\"cargoVolume\":\"7\"}";
 
-        mvc.perform(post("/cars/add")
+        MvcResult result = mvc.perform(post("/cars/add")
                 .accept(MediaType.APPLICATION_JSON).content(exampleCarJson)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        id=content.substring(7,31);
+        System.out.println(id);
+    }
+
+    @Test
+    @Order(3)
+    public void giveID_getSpecificCar_success() throws Exception {
+        mvc.perform(get("/cars/"+id).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.id",is(id)));
+    }
+
+    @Test
+    @Order(4)
+    public void giveID_updateCar() throws Exception {
+        String exampleUpdateCarJson ="{\"brand\":\"Toyota\",\"color\":\"White\",\"model\":\"Tesla\",\"body\":\"Sport\",\"combustible\":\"Electric\",\"owner\":\"null\",\"price\":\"1200\",\"numberDoors\":\"5\",\"cargoVolume\":\"7\"}";
+        mvc.perform(put("/cars/edit/"+id)
+                        .accept(MediaType.APPLICATION_JSON).content(exampleUpdateCarJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andDo(print());
-//                .andExpect(jsonPath("$.items",hasSize(2)));
+
+        System.out.println("Updated car id="+id);
+    }
+
+    @Test
+    @Order(5)
+    public void giveID_deleteCar() throws Exception {
+        mvc.perform(delete("/cars/delete/"+id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        System.out.println("Deleted car id="+id);
+    }
+
+    @Test
+    @Order(6)
+    public void giveID_getSpecificCar_failure() throws Exception {
+        mvc.perform(get("/cars/"+id).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
     }
 }
