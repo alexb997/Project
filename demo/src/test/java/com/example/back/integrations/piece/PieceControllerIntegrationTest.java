90d2886f17ff7dad4ee872b8fbef4d8bef078547
alexb997 <alex_alexandru_997@yahoo.ro>
2022-05-07 20:33:14 +0300

    piecesControllerTest

---
+++ demo/src/test/java/com/example/back/integrations/piece/PieceControllerIntegrationTest.java
@@ -0,0 +1,105 @@
+package com.example.back.integrations.piece;
+
+import com.example.back.controller.PiecesController;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class PieceControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private PiecesController piecesController;
+
+    static String id= StringUtils.EMPTY;
+
+    @Test
+    @Order(1)
+    public void getPiecesTest() throws Exception {
+        mvc.perform(get("/pieces/all").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    @Test
+    @Order(2)
+    public void createPiece_thenStatusCreatedTest() throws Exception {
+        String examplePieceJson ="{\"name\":\"Anvelope-iarna\",\"model\":\"Mercedes-benz\",\"utility\":\"Piese-auto\",\"type\":\"Anvelope\",\"owner\":\"Tester121\",\"price\":\"121\"}";
+
+        MvcResult result = mvc.perform(post("/pieces/add")
+                        .accept(MediaType.APPLICATION_JSON).content(examplePieceJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        id=content.substring(7,31);
+        System.out.println(id);
+    }
+
+    @Test
+    @Order(3)
+    public void giveID_getSpecificPiece_successTest() throws Exception {
+        mvc.perform(get("/pieces/"+id).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.id",is(id)));
+    }
+
+    @Test
+    @Order(4)
+    public void giveID_updatePieceTest() throws Exception {
+        String exampleUpdatePieceJson ="{\"name\":\"Anvelope-vara\",\"model\":\"Volvo\",\"utility\":\"Piese-auto\",\"type\":\"Anvelope\",\"owner\":\"Tester121\",\"price\":\"121\"}";
+        mvc.perform(put("/pieces/edit/"+id)
+                        .accept(MediaType.APPLICATION_JSON).content(exampleUpdatePieceJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+        System.out.println("Updated piece id="+id);
+    }
+
+    @Test
+    @Order(5)
+    public void giveID_deletePieceTest() throws Exception {
+        mvc.perform(delete("/pieces/delete/"+id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        System.out.println("Deleted piece id="+id);
+    }
+
+    @Test
+    @Order(6)
+    public void giveID_getSpecificPiece_failureTest() throws Exception {
+        mvc.perform(get("/pieces/"+id).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+}
